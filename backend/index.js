b81aef57a6e36bc0ff658bcb9ec1637b4d55d815
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-12 01:23:02 +0530

    added signup functionalities

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { dbConnect } from "./config/dbconfig.js";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -9,8 +10,12 @@ dbConnect();
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+
+app.use("/api/auth", authRoutes);
