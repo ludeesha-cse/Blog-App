201e95a7c7c6e33bb1d2ba3a6a70c2f635ae6683
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-13 00:32:05 +0530

    Create Signin api functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,60 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
     // return res.status(400).json({ message: "All fields are required" });
-    next(errorHandler(400, "All fields are required"))
+    next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 12);
 
   const newUser = new User({ username, email, password: hashedPassword }); //username=username, email=email, password=password
-  
+
   try {
     await newUser.save();
     res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+      return;
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(400, "Invalid credentials"));
+      return;
+    }
+
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    const { password: userPassword, ...userWithoutPassword } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(userWithoutPassword);
   } catch (error) {
     next(error);
   }
