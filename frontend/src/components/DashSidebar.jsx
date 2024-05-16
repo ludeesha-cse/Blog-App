04162f3ec352af29db7f7011d8eca43aeb926339
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-16 22:58:58 +0530

    Added sidebar of Dash board

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+export default function DashSidebar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab"); // http://localhost:5173/dashboard?tab=profile
+    //you get the value of tab from the url
+    console.log(tabFromUrl);
+    setTab(tabFromUrl);
+  }, [location.search]);
+
+  const handleNavigation = () => {
+    navigate("/dashboard?tab=profile");
+  };
+
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            onClick={handleNavigation}
+            active={tab === "profile"}
+            icon={HiUser}
+            label={"User"}
+            labelColor="dark"
+          >
+            Profile
+          </Sidebar.Item>
+
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab"); // http://localhost:5173/dashboard?tab=profile
+    //you get the value of tab from the url
+    console.log(tabFromUrl);
+    setTab(tabFromUrl);
+  }, [location.search]);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Side bar */}
+        <DashSidebar />
+      </div>
+      <div className="">
+        {/* profile */}
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
+  );
 }
