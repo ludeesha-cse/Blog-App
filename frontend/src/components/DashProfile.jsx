2fdd3107bdbf8b46c6b7fa06ba720c5400424346
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-17 09:56:34 +0530

    profile photo upload part 1

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,20 +1,82 @@
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [ imageFileUploadProgress, setImageFileUploadProgress ] = useState(0); // [1]
+  const [imageFileUploadError, setImageFileUploadError] = useState(null); // [2]
+  const filePickerRef = useRef();
+
+  console.log(imageFileUploadProgress, imageFileUploadError);
+  const hanldeImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2*1024*1024 &&
+    //       request.resource.contentType.matches('Image/.*');
+    //     }
+    //   }
+    // }
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+        
+      },
+      (error) =>
+        setImageFileUploadError("Failed to upload image. File must be less than 2MB"),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    )
+  };
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={hanldeImageChange}
+          ref={filePickerRef}
+          className="hidden"
+        />
         <div
           className="w-32 h-32 self-center cursor-pointer shadow-md 
                     overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
         >
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full border-8 border-[lightgray]"
           />
