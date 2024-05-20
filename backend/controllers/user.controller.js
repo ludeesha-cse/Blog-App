57202fbaec874ea75d488c357cee4cae64da427d
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-20 18:19:36 +0530

    added delete user api route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -47,3 +47,14 @@ export const updateUser = async (req, res, next) => {
   }
 }
 
+export const deleteUser = async (req, res, next) => {
+  if(req.user.id !== req.params.userId){
+    return next(errorHandler(403, "Unauthorized"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: "User deleted successfully" });
+  }catch(error){
+    next(error)
+  }
+}
