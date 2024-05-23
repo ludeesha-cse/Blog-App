eaaff1764fb9443c21a53006ed29cb98d2bb55f4
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2024-05-23 22:06:18 +0530

    added upload post image functinality

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,9 +1,61 @@
-import { Button, FileInput, Select, TextInput } from "flowbite-react";
-import React from "react";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError("Please select an image to upload");
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError("Failed to upload image");
+          setImageUploadProgress(null);
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({ ...formData, image: downloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError("Failed to upload image");
+      setImageUploadProgress(null);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl font-bold my-7">Create Post</h1>
@@ -27,15 +79,40 @@ export default function CreatePost() {
           className="flex gap-4 items-center justify-between border-4
          border-teal-500 border-dotted p-3"
         >
-          <FileInput type="file" id="image" accept="image/*" />
+          <FileInput
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
           <Button
             type="button"
             className="bg-gradient-to-r from-yellow-300 via-orange-500 to-pink-600"
             outline
+            onClick={handleUploadImage}
+            disabled={imageUploadProgress}
           >
-            Upload Image{" "}
+            {imageUploadProgress ? (
+              <div className="w-10 h-10">
+                <CircularProgressbar
+                  value={imageUploadProgress}
+                  text={`${imageUploadProgress || 0}%`}
+                  
+                />
+              </div>
+            ) : (
+              "Upload Image"
+            )}
           </Button>
         </div>
+        {imageUploadError && (
+          <Alert type="error" className="mt-4">
+            {imageUploadError}
+          </Alert>
+        )}
+        {formData.image && (
+          <img src={formData.image} alt="uploaded" className="w-full h-72" />
+        )}
         <ReactQuill
           theme="snow"
           id="content"
@@ -47,7 +124,7 @@ export default function CreatePost() {
           type="submit"
           className="bg-gradient-to-r from-yellow-300 via-orange-500 to-pink-600"
         >
-            Publish
+          Publish
         </Button>
       </form>
     </div>
