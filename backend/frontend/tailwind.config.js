1a4a6b028d2243fb754f65453f5566e892c00852
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-01-04 02:01:45 +0530

    Fix deployement issue with tailwind reqiure issue.

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content],
   theme: {
     extend: {},
   },
-  plugins: [
-    flowbite.plugin(),
-    require("tailwind-scrollbar"),
-  ],
+  plugins: [flowbite.plugin, tailwindScrollbar],
 };
