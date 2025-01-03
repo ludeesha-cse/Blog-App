67c80da5938e36d4ebb53bcb523234140aaa2c14
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-01-04 02:04:19 +0530

    Update Tailwind CSS configuration to include flowbite-react module for improved styling

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -3,7 +3,11 @@ import flowbite from "flowbite-react/tailwind";
 import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
