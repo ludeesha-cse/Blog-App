ddb81339662e01427a4de46dc6e757b47ee8e676
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-01-06 13:25:04 +0530

    Enhance Vite proxy configuration with error handling and logging for better debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,24 @@ export default defineConfig({
       "/api": {
         target: "https://blog-app-2ell.onrender.com",
         changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+
+        configure: (proxy, options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Request sent to target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Response received from target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
       },
     },
   },
