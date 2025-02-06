7c70fe0ec5ae0a7b17fbe5df3e12f2945631e091
Ludeesha Nanayakkara <ludeeshananayakkara.20@gmail.com>
2025-02-06 23:08:53 +0530

    Implement lazy loading for routes in App component, add LoadingSpinner, and create AuthService for user authentication

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import { lazy, Suspense } from 'react';
-import LoadingSpinner from './components/LoadingSpinner';
+import { lazy, Suspense } from "react";
+import LoadingSpinner from "./components/LoadingSpinner";
 
-const Home =  lazy(() => import("./pages/Home"));
-const About =  lazy(() => import("./pages/About"));
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
 const SignIn = lazy(() => import("./pages/SignIn"));
 const SignUp = lazy(() => import("./pages/SignUp"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
@@ -22,27 +22,27 @@ const Search = lazy(() => import("./pages/Search"));
 export default function App() {
   return (
     <BrowserRouter>
-    <Suspense fallback={<LoadingSpinner />}>
-    <ScrollToTop />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/search" element={<Search />} />
+      <Suspense fallback={<LoadingSpinner />}>
+        <ScrollToTop />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/search" element={<Search />} />
 
-        <Route element={<Privateroute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route element={<AdminRoute />}>
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/update-post/:postId" element={<UpdatePost />} />
-        </Route>
-        <Route path="/projects" element={<Project />} />
-        <Route path="/post/:postSlug" element={<PostPage />} />
-      </Routes>
-      <Footer />
+          <Route element={<Privateroute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route element={<AdminRoute />}>
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:postId" element={<UpdatePost />} />
+          </Route>
+          <Route path="/projects" element={<Project />} />
+          <Route path="/post/:postSlug" element={<PostPage />} />
+        </Routes>
+        <Footer />
       </Suspense>
     </BrowserRouter>
   );
--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -35,12 +35,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+
 export default defineConfig(({ mode }) => {
   return {
     server: mode === "development" && {
       proxy: {
         "/api": {
-          target: "https://blog-app-2ell.onrender.com",
+          target: process.env.VITE_API_URL || "https://blog-app-2ell.onrender.com",
           changeOrigin: true,
         },
       },
